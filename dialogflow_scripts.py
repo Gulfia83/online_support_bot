43e82c2268fdcb5d3cc0f462f613137a3cdce871
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-10-02 10:52:36 +0300

    Move dialogflow_scripts

---
+++ dialogflow_scripts.py
@@ -0,0 +1,48 @@
+from google.cloud import dialogflow
+
+
+def detect_intent_text(project_id, session_id, message_to_dialogflow, language_code='ru'):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=message_to_dialogflow, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    serialized_answer = {
+        'intention': response.query_result.intent.display_name,
+        'confidence': response.query_result.intent_detection_confidence,
+        'answer': response.query_result.fulfillment_text
+    }
+    return serialized_answer
+
+
+def create_intent(project_id,
+                  display_name,
+                  training_phrases_parts,
+                  message_texts):
+    intents_client = dialogflow.IntentsClient()
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(
+            text=training_phrases_part
+            )
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=[message_texts])
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
--- tg_bot.py
+++ tg_bot.py
@@ -1,55 +1,29 @@
 import logging
 
 from environs import Env
-from google.cloud import api_keys_v2
-from google.cloud import dialogflow
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import Update, ForceReply, Bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, \
+      CallbackContext
 
+from dialogflow_scripts import detect_intent_text
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
 
 logger = logging.getLogger(__name__)
 
-env = Env()
-env.read_env()
 
+class TelegramLogsHandler(logging.Handler):
 
-def create_api_key(project_id):
-    client = api_keys_v2.ApiKeysClient()
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
 
-    key = api_keys_v2.Key()
-    key.display_name = "My first API key"
-
-    request = api_keys_v2.CreateKeyRequest()
-    request.parent = f"projects/{project_id}/locations/global"
-    request.key = key
-
-    response = client.create_key(request=request).result()
-    return response
-
-
-def detect_intent_text(project_id, session_id, message_to_dialogflow, language_code='ru'):
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
-
-    text_input = dialogflow.TextInput(text=message_to_dialogflow, language_code=language_code)
-    query_input = dialogflow.QueryInput(text=text_input)
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-    serialized_answer = {
-        'intention': response.query_result.intent.display_name,
-        'confidence': response.query_result.intent_detection_confidence,
-        'answer': response.query_result.fulfillment_text
-    }
-    return serialized_answer
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
     user = update.effective_user
     update.message.reply_markdown_v2(
         fr'Hi {user.mention_markdown_v2()}\!',
@@ -57,29 +31,39 @@ def start(update: Update, context: CallbackContext) -> None:
     )
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
 def echo_dialogflow(update, context):
     message_to_dialogflow = update.message.text
     session_id = update.effective_chat.id
-    serialized_answer = detect_intent_text(project_id, session_id, message_to_dialogflow)
+    serialized_answer = detect_intent_text(
+        project_id,
+        session_id,
+        message_to_dialogflow
+        )
     update.message.reply_text(serialized_answer['answer'])
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':   
+    env = Env()
+    env.read_env()
     tg_bot_token = env.str('TG_BOT_TOKEN')
-    project_id = env.str('PROJECT_ID')
-    token = create_api_key(project_id)
-    print("Successfully created an API key")
+    tg_chat_id = env.str('TG_CHAT_ID')
+    project_id = env.str('GOOGLE_CLOUD_PROJECT_ID')
 
-    updater = Updater(tg_bot_token)
-    dispatcher = updater.dispatcher
+    bot = Bot(tg_bot_token)
+    logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(bot, tg_chat_id))
+    logger.info('Бот запущен')
 
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo_dialogflow))
+    try:
+        updater = Updater(tg_bot_token)
+        dispatcher = updater.dispatcher
 
-    updater.start_polling()
-    updater.idle()
+        dispatcher.add_handler(CommandHandler("start", start))
+        dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command,
+                                              echo_dialogflow))
+
+        updater.start_polling()
+        updater.idle()
+    except Exception as exception:
+        logging.exception(exception)
